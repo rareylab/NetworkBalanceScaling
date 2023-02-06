a6be4d5e2139778187a173e7b060bac1605a1be7
Wolf-Guido Bolick <wolf-guido.bolick@gmx.de>
2023-02-06 19:36:27 +0100

    utils/evaluate_results.py: fixed wrongly used dictionary

--- utils/evaluate_results.py
+++ utils/evaluate_results.py
@@ -7,7 +7,6 @@
 # Sophia Hönig, Wolf-Guido Bolick, Emanuel Ehmki, Matthias Rarey
 
 
-
 from rdkit import Chem
 import argparse
 import math
@@ -16,17 +15,60 @@ from sklearn.metrics import mean_squared_error, r2_score
 from statistics import stdev
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Calculate the RMSD value for a molecular property.')
-    parser.add_argument('--original', '-o', metavar='ORIGINAL', required=True, type=str, help='File with original property values. SDF or SMILES format.')
-    parser.add_argument('--prediction', '-p', metavar='PREDICTION', required=True, type=str, help='File with predicted property values. SDF or SMILES format.')
-    parser.add_argument('--optimized', '-q', metavar="OPTIMIZED", type=str, help="File with optimized property predictions. SDF or SMILES format.")
-    parser.add_argument('-i', '--id', type=str, default="_Name", help="ID tag for files in SD format.")
-    parser.add_argument('-y', '--id_prediction', type=str, default="_Name", help="ID tag for predicted SD file.")
-    parser.add_argument('-l', '--logfile', type=str, default="evaluation_result.log")
-    parser.add_argument('--label_predicted', type=str, required=True, help="Label of the prediction within the optimized file.")
-    parser.add_argument('--label_optimized', type=str, required=True, help="Label of the optimized prediction within the optimized file.")
-    
-    parser.add_argument('property', metavar='Property', type=str, help='Property of interest (should be similar in both files.')
+    parser = argparse.ArgumentParser(
+        description="Calculate the RMSD value for a molecular property."
+    )
+    parser.add_argument(
+        "--original",
+        "-o",
+        metavar="ORIGINAL",
+        required=True,
+        type=str,
+        help="File with original property values. SDF or SMILES format.",
+    )
+    parser.add_argument(
+        "--prediction",
+        "-p",
+        metavar="PREDICTION",
+        required=True,
+        type=str,
+        help="File with predicted property values. SDF or SMILES format.",
+    )
+    parser.add_argument(
+        "--optimized",
+        "-q",
+        metavar="OPTIMIZED",
+        type=str,
+        help="File with optimized property predictions. SDF or SMILES format.",
+    )
+    parser.add_argument(
+        "-i", "--id", type=str, default="_Name", help="ID tag for files in SD format."
+    )
+    parser.add_argument(
+        "-y",
+        "--id_prediction",
+        type=str,
+        default="_Name",
+        help="ID tag for predicted SD file.",
+    )
+    parser.add_argument("-l", "--logfile", type=str, default="evaluation_result.log")
+    parser.add_argument(
+        "--label_predicted",
+        type=str,
+        help="Label of the prediction within the optimized file.",
+    )
+    parser.add_argument(
+        "--label_optimized",
+        type=str,
+        help="Label of the optimized prediction within the optimized file.",
+    )
+
+    parser.add_argument(
+        "property",
+        metavar="Property",
+        type=str,
+        help="Property of interest (should be similar in both files.",
+    )
     args = parser.parse_args()
 
     original_dict = {}
@@ -52,7 +94,9 @@ if __name__ == "__main__":
     if pred_ext == ".sdf":
         pred_mols = Chem.SDMolSupplier(args.prediction)
         for mol in pred_mols:
-            pred_dict[mol.GetProp(args.id_prediction)] = float(mol.GetProp(args.label_predicted))
+            pred_dict[mol.GetProp(args.id_prediction)] = float(
+                mol.GetProp(args.label_predicted)
+            )
     else:
         for line in open(args.prediction, "r"):
             line_arr = line.split("\t")
@@ -62,21 +106,23 @@ if __name__ == "__main__":
         if os.path.splitext(args.optimized)[-1].lower() == ".sdf":
             pred_mols = Chem.SDMolSupplier(args.optimized)
             for mol in pred_mols:
-                opti_dict[mol.GetProp(id_label)] = float(mol.GetProp(args.label_optimized))
+                opti_dict[mol.GetProp(id_label)] = float(
+                    mol.GetProp(args.label_optimized)
+                )
         else:
-            for line in open(args.prediction, "r"):
+            for line in open(args.optimized, "r"):
                 line_arr = line.split("\t")
-                pred_dict[line_arr[1]] = float(line_arr[2]) + float(line_arr[3])
+                opti_dict[line_arr[1]] = float(line_arr[2]) + float(line_arr[3])
 
     sum_sq = 0
 
     preds = []
     orgs = []
-    optis_all = [] # including unoptimizable prediction values
+    optis_all = []  # including unoptimizable prediction values
     optis_only = []
     orgs_only = []
     unoptimizable_ids = []
-    
+
     unopt_values = []
     unopt_values_for_ori = []
 
@@ -86,7 +132,7 @@ if __name__ == "__main__":
     for id in pred_dict.keys():
         preds.append(pred_dict[id])
         orgs.append(original_dict[id])
-        diff = (pred_dict[id] - original_dict[id])
+        diff = pred_dict[id] - original_dict[id]
         if args.optimized:
             if id in opti_dict.keys():
                 optis_only.append(opti_dict[id])
@@ -116,24 +162,40 @@ if __name__ == "__main__":
         if args.optimized:
             f.write(f"original all values:              {stdDev_orig_all}\n")
             f.write(f"original only in net values:      {stdDev_orig_only_in_net}\n")
-            f.write(f"original only NOT in net values:  {stdDev_orig_only_not_in_net}\n")
+            f.write(
+                f"original only NOT in net values:  {stdDev_orig_only_not_in_net}\n"
+            )
 
             f.write(f"optimized all values:             {stdDev_opt_all}\n")
             f.write(f"optimized only in net values:     {stdDev_opt_only_in_net}\n\n")
 
-
-        f.write(f"                              Root Mean Square Deviation    R²        Num_of_Samples\n")
-        f.write(f"Predictions(all):             {mean_squared_error(orgs, preds, squared=False):.6f}                      {r2_score(orgs, preds):.6f}  {len(orgs)}\n")
+        f.write(
+            f"                              Root Mean Square Deviation    R²        Num_of_Samples\n"
+        )
+        f.write(
+            f"Predictions(all):             {mean_squared_error(orgs, preds, squared=False):.6f}                      {r2_score(orgs, preds):.6f}  {len(orgs)}\n"
+        )
         if args.optimized:
-            f.write(f"Predictions(only in net):     {mean_squared_error(orgs_only, pred_in_net, squared=False):.6f}                      {r2_score(orgs_only, pred_in_net):.6f}  {len(orgs_only)}\n")
-            f.write(f"Predictions(only NOT in net): {mean_squared_error(unopt_values_for_ori, pred_not_in_net, squared=False):.6f}                      {r2_score(unopt_values_for_ori, pred_not_in_net):.6f}  {len(unopt_values_for_ori)}\n\n")
+            f.write(
+                f"Predictions(only in net):     {mean_squared_error(orgs_only, pred_in_net, squared=False):.6f}                      {r2_score(orgs_only, pred_in_net):.6f}  {len(orgs_only)}\n"
+            )
+            f.write(
+                f"Predictions(only NOT in net): {mean_squared_error(unopt_values_for_ori, pred_not_in_net, squared=False):.6f}                      {r2_score(unopt_values_for_ori, pred_not_in_net):.6f}  {len(unopt_values_for_ori)}\n\n"
+            )
 
             if len(optis_all) > 0:
-                f.write(f"Optimized (all):              {mean_squared_error(orgs, optis_all, squared=False):.6f}                      {r2_score(orgs, optis_all):.6f}  {len(orgs)}\n")
                 f.write(
-                    f"Optimized (only):             {mean_squared_error(orgs_only, optis_only, squared=False):.6f}                      {r2_score(orgs_only, optis_only):.6f}  {len(orgs_only)}\n\n")
+                    f"Optimized (all):              {mean_squared_error(orgs, optis_all, squared=False):.6f}                      {r2_score(orgs, optis_all):.6f}  {len(orgs)}\n"
+                )
+                f.write(
+                    f"Optimized (only):             {mean_squared_error(orgs_only, optis_only, squared=False):.6f}                      {r2_score(orgs_only, optis_only):.6f}  {len(orgs_only)}\n\n"
+                )
             if len(unopt_values) > 0:
-                f.write(f"Scores (un_opt):              {mean_squared_error(unopt_values_for_ori, unopt_values, squared=False):.6f}                      {r2_score(unopt_values_for_ori, unopt_values):.6f}  {len(unopt_values)}\n")
-            f.write(f"\nUnoptimizable molecules (IDs) ({len(unoptimizable_ids)} mols):\n")
+                f.write(
+                    f"Scores (un_opt):              {mean_squared_error(unopt_values_for_ori, unopt_values, squared=False):.6f}                      {r2_score(unopt_values_for_ori, unopt_values):.6f}  {len(unopt_values)}\n"
+                )
+            f.write(
+                f"\nUnoptimizable molecules (IDs) ({len(unoptimizable_ids)} mols):\n"
+            )
             for id in unoptimizable_ids:
                 f.write(f"{id}\n")
